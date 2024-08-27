d7052b0b2d29fa84ebd4a24585fbf810341acefa
Sanket Y <124805576+sanketyelave@users.noreply.github.com>
2024-08-28 00:27:42 +0530

    Update CopyButton.js

--- src/app/id/[id]/CopyButton.js
+++ src/app/id/[id]/CopyButton.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 export const TwitterShareButton = () => {
-    const textToShare = "Check out this awesome content!" // The text you want to share
-    const urlToShare = "https://example.com" // The URL you want to share (optional)
-    const hashtags = "Example,React" // Comma-separated hashtags (optional)
+    const textToShare = "Check out this awesome content!"
+    const urlToShare = "https://example.com"
+    const hashtags = "Example,React"
 
     const handleTwitterShareClick = () => {
         const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
